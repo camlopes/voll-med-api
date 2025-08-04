5d734e924d9c241ce774b8c73941f3b959a76b33
Camila Lopes <63123059+camlopes@users.noreply.github.com>
2025-08-04 14:24:35 -0300

    Testes automatizados do controller de medico para os codigos http 400 e 201

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
